c2c77cb9f27cdfad9ffcfa3e9287bd93ecbdf894
Vincent <vincentochieng33@gmail.com>
2019-11-21 09:53:45 +0300

    initial commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -188,7 +188,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            const secondArray=firstArray;
             ////////////////////////////////////////////////////////////////////
 
             expect(secondArray).to.eql([1, 2, 3]);

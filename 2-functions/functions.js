8e1311c0d4fedc2d7c09991eb34653676999e0af
Vincent <vincentochieng33@gmail.com>
2019-11-29 16:10:46 +0300

    initial commit

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -277,7 +277,7 @@ describe("functions", function() {
                 // This means that we can't change it.
                 // Is there a way to still make 'myConst' equal true here?
                 // ✏️ ADD CODE HERE ✏️
-
+                return arguments.length;
                 ////////////////////////////////////////////////////////////////////
 
                 if (myConst !== true) {
